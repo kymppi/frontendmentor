19c67d224441d92b88de457853258d7c5cd5efc0
Midka <me@midka.dev>
2023-01-06 19:46:36 +0200

    add: current sketch of order summary

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,13 @@ module.exports = {
         '7b879d': '#7b879d',
         d6e2f0: '#d6e2f0',
         '1f3251': '#1f3251',
+        f5f7ff: '#f5f7ff',
+        e0e8ff: '#e0e8ff',
+        '1f2f56': '#1f2f56',
+        '3829e0': '#3829e0',
+      },
+      fontFamily: {
+        redhatdisplay: 'Red Hat Display',
       },
     },
   },
