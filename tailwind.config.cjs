3c7fe19afd4ced62b5e42363005465305c40923f
Midka <me@midka.dev>
2023-01-06 18:21:50 +0200

    add: qr-code-component challenge

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        '7b879d': '#7b879d',
+        d6e2f0: '#d6e2f0',
+        '1f3251': '#1f3251',
+      },
+    },
+  },
+  plugins: [],
+};
